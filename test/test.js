ab0e05df024ec59ef849569c629e1c66a45ab879
Diego F.(i7) <diegoefe@gmail.com>
2016-05-31 10:35:52 -0300

    Subo esqueleto para test

---
+++ test/test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var Path = require('path');
+
+var expect = require("expect.js");
+var fs = require("fs-promise");
+//var semver = require('semver');
+var basicInterfaces = require("..");
+
+describe("basic-interfaces", function(){
+    it("must generate js source code from comment nodes", function(){
+        expect(true).to.be.ok();
+    });
+});
+
